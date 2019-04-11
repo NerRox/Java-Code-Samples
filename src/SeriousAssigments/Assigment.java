c077261519a9f53380c17c6d012bc190b7d9f80a
Andrey MOU Larionov <anlarionov@gmail.com>
2019-04-11 17:46:22 +0300

    Small fix to launch

--- src/SeriousAssigments/Assigment.java
+++ src/SeriousAssigments/Assigment.java
@@ -7,25 +7,20 @@ import java.util.*;
 import java.util.regex.*;
 
 public class Assigment {
-    private static DateFormat dateFormat; // = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH);
-    private static String regex; // = "^(?:[^\\s]+\\s){3}\\[([^\\]]+)\\].*$";
-    private static String targetDate;
+    private String filename;
+    private DateFormat dateFormat; // = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH);
+    private String regex; // = "^(?:[^\\s]+\\s){3}\\[([^\\]]+)\\].*$";
+    private String targetDate;
 
     private Assigment(String[] args) {
+        filename = args[0];
         dateFormat = new SimpleDateFormat(args[1], Locale.ENGLISH);
         targetDate = args[3];
         regex = args[2];
     }
 
-    public static void main(String[] args) throws Exception {
-        Assigment assigment = new Assigment(args);
-//        Static file on hard drive
-//        String filePath = "." + File.separator + "src" + File.separator + "log.log";
-//        File fileToRead = new File(filePath);
-
-
-//        Creating file with exists() check
-        File fileToRead = new File(args[0]);
+    private void run() throws Exception {
+        File fileToRead = new File(filename);
 
         if (fileToRead.exists()) {
             fileToRead = fileToRead.getCanonicalFile();
@@ -36,7 +31,6 @@ public class Assigment {
             throw new IOException();
         }
 
-//        String targetDate = "27/Dec/2015:14:18:20 +0100";
         Date staticDate =  dateFormat.parse(targetDate);
 
         try(RandomAccessFile file = new RandomAccessFile(fileToRead, "r")) {
@@ -66,9 +60,10 @@ public class Assigment {
                 }
             }
         }
+
     }
 
-    private static long getLineEnd(RandomAccessFile file, long middle, long right) throws IOException {
+    private long getLineEnd(RandomAccessFile file, long middle, long right) throws IOException {
         long pos = middle;
         file.seek(pos);
         while (pos < right) {
@@ -80,7 +75,7 @@ public class Assigment {
         return right;
     }
 
-    private static long getLineStart(RandomAccessFile file, long middle, long left) throws IOException {
+    private long getLineStart(RandomAccessFile file, long middle, long left) throws IOException {
         long pos = middle;
         while (pos >= left) {
             file.seek(pos);
@@ -92,8 +87,7 @@ public class Assigment {
         return left;
     }
 
-    private static Date strDateParser(String str) throws Exception {
-//        String dateFromStr = str.substring(str.indexOf("[") + 1, str.indexOf("]"));
+    private Date strDateParser(String str) throws Exception {
         String dateFromStr;
         Pattern pattern = Pattern.compile(regex);
         try {
@@ -108,25 +102,8 @@ public class Assigment {
         return dateFormat.parse(dateFromStr);
     }
 
-    public static int binarySearch(int[] arr, int x) {
-        int left = 0, right = arr.length - 1;
-
-        while (left <= right) {
-            int middle = left + (right - left) / 2;
-
-            if (arr[middle] == x) {
-                return middle;
-            }
-
-            if (arr[middle] < x) {
-                left = middle + 1;
-            }
-
-            else {
-                right = middle - 1;
-            }
-        }
-
-        return -1;
+    public static void main(String[] args) throws Exception {
+        Assigment assigment = new Assigment(args);
+        assigment.run();
     }
 }
